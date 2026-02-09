7a85fdbbb9f83abe4170302fd6d69af3aa472517
jjnix-john <johneltondgeromo438@gmail.com>
2026-02-09 20:02:17 +0800

    added users/ about and shop

---
+++ resources/views/user/contacts.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contacts</h1>
+    <p>For any inquiries or support, please contact us at:</p>
+    <ul>
+        <li>Email:
+            <a href="mailto:contact@biddie.com">contact@biddie.com</a>
+        </li>
+        <li>Phone: +1 (555) 123-4567</li>
+        <li>Address: 123 Biddie Street, Auction City, USA</li>
+
+    </ul>
+    
+</body>
+</html>
